ba4df5fc517aa6d4a9ba9c37dd64d1ec40a2d01b
sveri <sveri80@gmail.com>
2015-04-12 09:38:13 +0200

    even better

--- src/leiningen/pre_types.clj
+++ src/leiningen/pre_types.clj
@@ -1,18 +1,19 @@
 (ns leiningen.pre-types
-  (:require [clojure.core.typed :as t :refer [defalias HMap HSeq]])
+  (:refer-clojure :exclude [fn])
+  (:require [clojure.core.typed :as t :refer [defalias HMap HSeq fn]])
   (:import (clojure.lang Keyword)))
 
 (defalias entity-description  (HMap :mandatory {:name String :columns (HSeq [(HSeq [Keyword Keyword *])
                                                                              (HSeq [Keyword Keyword *]) *])}))
 
 
-(def cols [[:id "foo"] [:fooname "foo"]])
-
-(t/ann mapf [(t/HSequential [Keyword t/Any *]) -> (t/HMap :mandatory {:colname String})])
-(defn mapf [v]
-  {:colname (name (first v))})
+;(def cols [[:id "foo"] [:fooname "foo"]])
+;
+;(t/ann mapf [(t/HSequential [Keyword t/Any *]) -> (t/HMap :mandatory {:colname String})])
+;(defn mapf [v]
+;  {:colname (name (first v))})
 
 (t/ann conv [(t/NonEmptyVec (t/HSequential [t/Kw t/Any *]))
              -> (t/NonEmptyVec (t/HMap :mandatory {:colname String}))])
 (defn conv [cols]
-  (mapv mapf cols))
+  (mapv (fn [col :- (t/HSequential [Keyword t/Any *])] {:colname (name (first col))}) cols))
