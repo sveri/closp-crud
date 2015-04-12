f227fb2b7d6ecc7d8083cd433f4655409ededfbd
sveri <sveri80@gmail.com>
2015-04-12 10:31:18 +0200

    phew...types!

--- src/leiningen/code_generator.clj
+++ src/leiningen/code_generator.clj
@@ -6,44 +6,33 @@
             [de.sveri.clojure.commons.files.faf :as faf])
   (:import (clojure.lang Keyword Seqable)))
 
-(ann filter-id-columns [(t/HVec [(t/HSeq [t/Kw t/Any])]) ->
-                        (t/Option (clojure.lang.Seqable t/Any))
-                        ])
 
-
+(ann filter-id-columns [(t/NonEmptySeq (t/HSequential [Keyword t/Any *]))
+                        -> (t/Option (Seqable (t/HSequential [Keyword t/Any *])))])
 (defn filter-id-columns [cols]
-  (remove (t/fn [v :- (Seqable t/Any)] (= :id (first v))) cols))
+  (remove (t/fn [col :- (t/HSequential [Keyword t/Any *])] (= :id (first col))) cols))
 
-(ann ds-columns->template-columns [(t/HVec [(t/HSeq [t/Kw t/Any])]) ->
-                                   (t/HVec [(t/HMap :mandatory {:colname String})])
-                        ;(t/Option (clojure.lang.Seqable t/Any))
-                        ])
-;[{:colname "foo" } {:colname "bar"}]
+(ann ds-columns->template-columns [(t/NonEmptySeq (t/HSequential [Keyword t/Any *]))
+                                   -> (t/Option (Seqable (t/HMap :mandatory {:colname String})))])
 (defn ds-columns->template-columns [cols]
-  (mapv (t/fn [v :- (t/HSeq [t/Kw t/Any])] {:colname (name (first v))}) (filter-id-columns cols)))
+  (let [filt-cols (filter-id-columns cols)]
+    (mapv (t/fn [col :- (t/HSequential [Keyword t/Any *])] {:colname (name (first col))}) filt-cols)))
 
-;(All [c a b ...]
-     ;(t/IFn [[a b ... b -> c] (t/NonEmptySeqable a) (t/NonEmptySeqable b) ... b -> (t/NonEmptyAVec c)]
-     ;       [[a b ... b -> c] (t/U (Seqable a) nil) (t/U (Seqable b) nil) ... b -> (t/AVec c)]))
 
-;(ann ^:no-check dataset->template-map [String pt/entity-description ->
-;                            (t/HMap :mandatory {:entityname String
-;                                                :ns         String
-;                                                :cols       (t/HSeq [t/Any])})])
 (ann dataset->template-map [String pt/entity-description -> t/Any])
 (defn dataset->template-map
   "Will remove every column with the name :id"
   [ns ds]
   (let [cols (ds-columns->template-columns (:columns ds))]
     {:entityname       (:name ds)
-     :entityname-upper (.toUpperCase (:name ds))
+     :entityname-upper (.toUpperCase ^String (:name ds))
      :ns               (str ns "." (:name ds))
      :cols             cols}))
-
-(ann render-db-file [String pt/entity-description -> String])
-(defn render-db-file [ns dataset]
-  (let [templ-map (dataset->template-map ns dataset)]
-    (stenc/render-file "templates/db.mustache" templ-map)))
+;
+;(ann ^:no-check render-db-file [String pt/entity-description -> String])
+;(defn render-db-file [ns dataset]
+;  (let [templ-map (dataset->template-map ns dataset)]
+;    (stenc/render-file "templates/db.mustache" templ-map)))
 
 ;(defn store-db-file [ns string filename proj-fp]
 ;  (let [ns-path (str proj-fp "/" (s/replace ns #"\." "/"))
--- src/leiningen/pre_types.clj
+++ src/leiningen/pre_types.clj
@@ -1,19 +1,21 @@
 (ns leiningen.pre-types
   (:refer-clojure :exclude [fn])
   (:require [clojure.core.typed :as t :refer [defalias HMap HSeq fn]])
-  (:import (clojure.lang Keyword)))
+  (:import (clojure.lang Keyword Seqable)))
 
 (defalias entity-description  (HMap :mandatory {:name String :columns (HSeq [(HSeq [Keyword Keyword *])
                                                                              (HSeq [Keyword Keyword *]) *])}))
 
 
-;(def cols [[:id "foo"] [:fooname "foo"]])
-;
-;(t/ann mapf [(t/HSequential [Keyword t/Any *]) -> (t/HMap :mandatory {:colname String})])
-;(defn mapf [v]
-;  {:colname (name (first v))})
+(def cols [[:id "foo"] [:fooname "foo"]])
 
-(t/ann conv [(t/NonEmptyVec (t/HSequential [t/Kw t/Any *]))
-             -> (t/NonEmptyVec (t/HMap :mandatory {:colname String}))])
+(t/ann filter-ids [(t/NonEmptyVec (t/HSequential [Keyword t/Any *]))
+                   -> (t/Option (Seqable (t/HSequential [Keyword t/Any *])))])
+(defn filter-ids [cols]
+  (remove (fn [col :- (t/HSequential [Keyword t/Any *])] (= :id (first col))) cols))
+
+(t/ann conv [(t/NonEmptyVec (t/HSequential [Keyword t/Any *]))
+             -> (t/Option (Seqable (t/HMap :mandatory {:colname String})))])
 (defn conv [cols]
-  (mapv (fn [col :- (t/HSequential [Keyword t/Any *])] {:colname (name (first col))}) cols))
+  (let [f-cols (filter-ids cols)]
+      (mapv (fn [col :- (t/HSequential [Keyword t/Any *])] {:colname (name (first col))}) f-cols)))
