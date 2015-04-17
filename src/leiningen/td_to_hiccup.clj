db84b7ad7d7cace39657da7af08652eba83b0e81
sveri <sveri80@gmail.com>
2015-04-17 15:35:39 +0200

    types types types

--- src/leiningen/td_to_hiccup.clj
+++ src/leiningen/td_to_hiccup.clj
@@ -6,12 +6,18 @@
 
 
 (defalias html-label (t/HVec [Keyword (t/HMap :mandatory {:for String}) String]))
-(defalias html-form (t/HVec [Keyword (t/HMap :mandatory {:id String}) t/Any *]))
+(defalias html-form (t/HVec [Keyword (t/HMap :mandatory {:id String}
+                                             :optional {:required String}
+                                             :complete? false) t/Any *]))
 (defalias html-form-group (t/HVec [html-label html-form]))
 
-(t/ann wrap-with-vec-and-label [pt/et-column html-form -> html-form-group])
-(defn wrap-with-vec-and-label [col hicc-col]
-  [(let [n (name (first col))] [:label {:for n} n]) hicc-col])
+;(t/ann wrap-with-vec-and-label [pt/et-column html-form -> html-form-group])
+;(defn wrap-with-vec-and-label [col hicc-col]
+;  [(let [n (name (first col))] [:label {:for n} n]) hicc-col])
+
+(t/ann wrap-with-vec-and-label [html-form -> (t/HVec [html-label])])
+(defn wrap-with-vec-and-label [hicc-col]
+  [(let [n "staire"] [:label {:for n} n])])
 
 (t/ann merge-required [(t/HMap :complete? false) pt/et-column -> (t/HMap :complete? false)])
 (defn merge-required [m col]
@@ -19,20 +25,17 @@
     (merge m {:required "required"})
     m))
 
-(defalias t (t/HVec [Keyword (t/U Keyword (t/HVec [Keyword Number])) t/Any t/Any *]))
+(t/ann dt->hiccup [(t/HVec [Keyword (t/U Keyword (t/HVec [Keyword Number])) t/Any t/Any *]) -> html-form-group])
+(defmulti dt->hiccup (t/fn [col :- (t/HVec [Keyword (t/U Keyword (t/HVec [Keyword Number])) t/Any t/Any *])]
+                       (let [[_ s] col]
+                         (if (vector? s) (first s) s))))
 
-;(t/ann dt->hiccup [pt/et-column -> (t/HVec [Keyword (t/HMap :mandatory {:id String}) (t/Option String)])])
-;(HVec [Keyword (U Keyword (HVec [Keyword Number])) t/Any t/Any *])
-;(t/ann ^:no-check dt->hiccup [pt/et-column -> html-form-group])
-(t/ann dt->hiccup [t -> t/Any])
-(defmulti dt->hiccup (t/fn [col :- t] :- Keyword
-                       (if (seq? (second col))
-                         (first (second col))
-                         (second col))))
+(defmethod dt->hiccup :int [col]
+  (wrap-with-vec-and-label [:input.form-control {:id (name (first col))}])
+  ;(wrap-with-vec-and-label [:input.form-control (merge-required {:id (name (first col))} col)])
+  ;(wrap-with-vec-and-label col [:input.form-control (merge-required {:id (name (first col))} col)])
+  )
 
-;(defmethod dt->hiccup :int [col]
-;  (wrap-with-vec-and-label col [:input.form-control (merge-required {:id (name (first col))} col)]))
-;
 ;(defmethod dt->hiccup :varchar [col]
 ;  {:pre [(second (second col))]}
 ;  (wrap-with-vec-and-label
