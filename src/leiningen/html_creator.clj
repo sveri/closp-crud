b24d1b5a86837895d482ee26d21245de809f0158
sveri <sveri80@gmail.com>
2015-04-18 06:50:20 +0200

    types types types

--- src/leiningen/html_creator.clj
+++ src/leiningen/html_creator.clj
@@ -1,8 +1,13 @@
 (ns leiningen.html-creator
   (:require [hiccup.core :as hicc]
-            [clojure.core.typed :as t]))
+            [clojure.core.typed :as t]
+            [leiningen.pre-types :as pt]
+            [leiningen.td-to-hiccup :as ds-conv]))
 
 (t/ann wrap-with-form-group [])
 (defn wrap-with-form-group [col-vec]
   (vec (concat [:div.form-group] col-vec)))
 
+(t/ann create-html [pt/entity-description -> String])
+(defn create-html [dataset]
+  )
--- src/leiningen/td_to_hiccup.clj
+++ src/leiningen/td_to_hiccup.clj
@@ -4,53 +4,48 @@
   (:import (clojure.lang Keyword)))
 
 
-
 (defalias html-label (t/HVec [Keyword (t/HMap :mandatory {:for String}) String]))
-(defalias html-form (t/HVec [Keyword (t/HMap :mandatory {:id String}
-                                             :optional {:required String}
-                                             :complete? false) t/Any *]))
+(defalias form-map (t/HMap :mandatory {:id String}
+                           :optional {:required String}
+                           :complete? false))
+(defalias html-form (t/HVec [Keyword form-map t/Any *]))
 (defalias html-form-group (t/HVec [html-label html-form]))
 
-;(t/ann wrap-with-vec-and-label [pt/et-column html-form -> html-form-group])
-;(defn wrap-with-vec-and-label [col hicc-col]
-;  [(let [n (name (first col))] [:label {:for n} n]) hicc-col])
+(t/ann wrap-with-vec-and-label [pt/et-column html-form -> html-form-group])
+(defn wrap-with-vec-and-label [col hicc-col]
+  [(let [n (name (first col))] [:label {:for n} n]) hicc-col])
 
-(t/ann wrap-with-vec-and-label [html-form -> (t/HVec [html-label])])
-(defn wrap-with-vec-and-label [hicc-col]
-  [(let [n "staire"] [:label {:for n} n])])
-
-(t/ann merge-required [(t/HMap :complete? false) pt/et-column -> (t/HMap :complete? false)])
+(t/ann merge-required [form-map pt/et-column -> form-map])
 (defn merge-required [m col]
   (if (and (nth col 2) (= (nth col 2) :null))
     (merge m {:required "required"})
     m))
 
-(t/ann dt->hiccup [(t/HVec [Keyword (t/U Keyword (t/HVec [Keyword Number])) t/Any t/Any *]) -> html-form-group])
+(t/ann ^:no-check dt->hiccup [(t/HVec [Keyword (t/U Keyword (t/HVec [Keyword Number])) t/Any t/Any *]) -> html-form-group])
 (defmulti dt->hiccup (t/fn [col :- (t/HVec [Keyword (t/U Keyword (t/HVec [Keyword Number])) t/Any t/Any *])]
                        (let [[_ s] col]
                          (if (vector? s) (first s) s))))
 
 (defmethod dt->hiccup :int [col]
-  (wrap-with-vec-and-label [:input.form-control {:id (name (first col))}])
-  ;(wrap-with-vec-and-label [:input.form-control (merge-required {:id (name (first col))} col)])
-  ;(wrap-with-vec-and-label col [:input.form-control (merge-required {:id (name (first col))} col)])
-  )
+  (wrap-with-vec-and-label col [:input.form-control (merge-required {:id (name (first col))} col)]))
 
-;(defmethod dt->hiccup :varchar [col]
-;  {:pre [(second (second col))]}
-;  (wrap-with-vec-and-label
-;    col
-;    [:input.form-control (merge-required {:id (name (first col)) :maxlength (second (second col))} col)]))
-;
-;(defmethod dt->hiccup :char [col]
-;  (dt->hiccup (assoc col 1 (assoc (second col) 0 :varchar))))
-;
-;(defmethod dt->hiccup :boolean [col]
-;  (wrap-with-vec-and-label
-;    col
-;    (let [col-m (apply assoc (sorted-map) col)]
-;      [:input.form-control (merge (when (= true (:default col-m)) {:checked "checked"})
-;                                  (merge-required {:id (name (first col))} col))])))
-;
-;(defmethod dt->hiccup :default [col]
-;  (wrap-with-vec-and-label col [:input.form-control (merge-required {:id (name (first col))} col)]))
+(defmethod dt->hiccup :varchar [col]
+  {:pre [(second (second col))]}
+  (wrap-with-vec-and-label
+    col
+    [:input.form-control (merge-required {:id (name (first col)) :maxlength (nth
+                                                                              (nth col 1)
+                                                                              1)} col)]))
+
+(defmethod dt->hiccup :char [col]
+  (dt->hiccup (assoc col 1 (assoc (second col) 0 :varchar))))
+
+(defmethod dt->hiccup :boolean [col]
+  (wrap-with-vec-and-label
+    col
+    (let [col-m (apply assoc (sorted-map) col)]
+      [:input.form-control (merge (when (= true (:default col-m)) {:checked "checked"})
+                                  (merge-required {:id (name (first col))} col))])))
+
+(defmethod dt->hiccup :default [col]
+  (wrap-with-vec-and-label col [:input.form-control (merge-required {:id (name (first col))} col)]))
