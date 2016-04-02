713b98922f694fe9f4f6a68a4957ef4816cac46e
sveri <sveri80@gmail.com>
2016-04-02 12:15:46 +0200

    adding value for every input

--- src/leiningen/td_to_hiccup.clj
+++ src/leiningen/td_to_hiccup.clj
@@ -15,15 +15,19 @@
   [(let [n (name (first col))] [:label {:for n} n]) hicc-col])
 
 (t/ann add-required-attr [pt/form-map String pt/et-column -> pt/form-map])
-(defn add-required-attr [m ent-name col]
+(defn add-required-attr [m col]
   (if (and (< 2 (count col)) (= (nth col 2) :null))
-    (merge m {:required "required" :value (str "{{" ent-name "." (name (first col)) "}}")})
+    (merge m {:required "required"})
     m))
 
+(defn add-value [m ent-name col]
+  (merge m {:value (str "{{" ent-name "." (name (first col)) "}}")}))
+
 (defn- wrap-with-vec-and-label [form-key col ent-name]
-  (wrap-with-vec-and-label' col [form-key (add-required-attr {:id   (name (first col))
-                                                              :name (name (first col))}
-                                                             ent-name col)]))
+  (wrap-with-vec-and-label' col [form-key (-> {:id   (name (first col))
+                                               :name (name (first col))}
+                                              (add-required-attr col)
+                                              (add-value ent-name col))]))
 
 (t/ann ^:no-check dt->hiccup [pt/et-column Keyword -> pt/html-form-group])
 (defmulti dt->hiccup
@@ -35,18 +39,20 @@
   (let [field-name (name (first col))]
     (wrap-with-vec-and-label' col
                               [:input.form-control
-                               (add-required-attr {:id   field-name
-                                                   :name field-name}
-                                                  ent-name col)])))
+                               (-> {:id   field-name
+                                    :name field-name}
+                                   (add-required-attr col)
+                                   (add-value ent-name col))])))
 
 (defmethod dt->hiccup [:varchar :create] [col ent-name _]
   {:pre [(second (second col))]}
   (wrap-with-vec-and-label'
     col
-    [:input.form-control (add-required-attr {:id        (name (first col))
-                                             :name      (name (first col))
-                                             :maxlength (nth (nth col 1) 1)}
-                                            ent-name col)]))
+    [:input.form-control (-> {:id        (name (first col))
+                              :name      (name (first col))
+                              :maxlength (nth (nth col 1) 1)}
+                             (add-required-attr col)
+                             (add-value ent-name col))]))
 
 (defmethod dt->hiccup [:char :create] [col ent-name _]
   (dt->hiccup (assoc col 1 (assoc (second col) 0 :varchar)) ent-name :create))
@@ -65,9 +71,10 @@
 
 (defmethod dt->hiccup [:text :create] [col ent-name _]
   (let [field-name (name (first col))]
-    (wrap-with-vec-and-label' col [:textarea.form-control (add-required-attr {:id   field-name
-                                                                              :name field-name}
-                                                                             ent-name col)
+    (wrap-with-vec-and-label' col [:textarea.form-control (-> {:id   field-name
+                                                               :name field-name}
+                                                              (add-required-attr col)
+                                                              (add-value ent-name col))
                                    (str "{{" ent-name "." field-name "}}")])))
 
 (defmethod dt->hiccup :default [col ent-name _]
